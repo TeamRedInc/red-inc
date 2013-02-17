37a76d65548bb456a0374614cc8a1f67ab313cf5
Alvin Yan <maretard@gmail.com>
2013-02-17 13:40:42 -0500

    Basic project structure

---
+++ Core/Core.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace core
+{
+    public class Core
+    {
+        /// <summary>
+        /// Primary thread of execution
+        /// All logic starts here!
+        /// </summary>
+        public Core()
+        {
+            
+        }
+    }
+}
