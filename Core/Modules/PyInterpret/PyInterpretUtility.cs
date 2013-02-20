61b13bb8300953e85741bc49b45c3e0d6caa7565
Alvin Yan <maretard@gmail.com>
2013-02-20 02:04:08 -0500

    Basic IronPython integration, project cleanup

---
+++ Core/Modules/PyInterpret/PyInterpretUtility.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IronPython.Hosting;
+using Microsoft.Scripting;
+using Microsoft.Scripting.Hosting;
+
+namespace core.Modules.PyInterpret
+{
+    public class PyInterpretUtility
+    {
+        private ScriptEngine _engine;
+        private ScriptScope _scope;
+
+        public PyInterpretUtility()
+        {
+            _engine = Python.CreateEngine();
+            _scope = null;
+        }
+
+        public string PyInterpret(string code)
+        {
+            _scope = _engine.CreateScope();
+            string output = "";
+            _scope.SetVariable("output",output);
+            _engine.Execute(code, _scope);
+            return _scope.GetVariable("output");
+        }
+    }
+}
